4f4ddcd8cb72a1f4d15b1142607264ed88e71312
mrmrh <max7hansen@hotmail.com>
2024-02-28 12:54:23 +0100

    New db.properties file and usage in class.

--- app/src/main/java/fridge/tertainment/sqlConnector/SqlConnectorTest.java
+++ app/src/main/java/fridge/tertainment/sqlConnector/SqlConnectorTest.java
@@ -1,25 +1,52 @@
 package fridge.tertainment.sqlConnector;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.*;
+import java.util.Properties;
+
+//Class to test datacase/code connection. Connection parameters are specified in db.properties file.
 
 public class SqlConnectorTest {
 
+    public static Properties properties = new Properties();
+    public static Connection connection = new Connection() {
+        
+    };
+
     public static void main(String[] args) {
+        getConnectionProperties();
+        initConnection(properties);
+        closeConnection(connection);;
+    }
+
+    public static final void getConnectionProperties(){
+        try (InputStream inputStream = new FileInputStream("app/src/main/java/fridge/tertainment/rsc/db.properties")){
+
+            properties.load(inputStream);
+
+        } catch (IOException ex){
+            ex.printStackTrace();
+        }
+    }
+
+    public static void initConnection(Properties properties){
         try
             {
                 Class.forName("com.mysql.cj.jdbc.Driver");
-                Connection connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/fridgeDB", "root", "gruppe6aau");
-                Statement statement = connection.createStatement();
-                ResultSet rSet = statement.executeQuery("SELECT food FROM foodTable;");
-                while (rSet.next()) {
-                    System.out.println(rSet.getString(1));
-                }
+                Connection connection=DriverManager.getConnection(properties.getProperty("jdbc.url"), properties.getProperty("jdbc.username"), properties.getProperty("jdbc.password"));
                 connection.close();
             }
             catch(Exception e){
-                System.out.println(e);    
+                e.printStackTrace();    
              }
-    
-}
-
+        }
+    public static void closeConnection(Connection connection){
+        try {
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
