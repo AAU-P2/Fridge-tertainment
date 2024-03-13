0fd20dcd545160a332cf126ab19e1f45b564e91a
Theis <theis99999@gmail.com>
2024-03-13 08:49:53 +0100

    Add Create(dto1 dto) to interface IDTO1Repository

--- app/src/main/java/fridge/tertainment/sqlConnector/IDTO1Repository.java
+++ app/src/main/java/fridge/tertainment/sqlConnector/IDTO1Repository.java
@@ -9,5 +9,7 @@ public interface IDTO1Repository<dto1> {
     boolean Delete(dto1 dto) throws SQLException;
     boolean Delete(int id) throws SQLException;
 
+    boolean Create(dto1 dto) throws SQLException;
+
     ArrayList<dto1> GetAll() throws SQLException;
 }
