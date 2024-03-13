86820f0ae3f7458647d84cad7c251184cc3c07e7
theis <theis99999@gmail.com>
2024-03-13 12:13:11 +0100

    Changed constructor of Repository to handle the updated repositories

--- app/src/main/java/fridge/tertainment/sqlConnector/Repository.java
+++ app/src/main/java/fridge/tertainment/sqlConnector/Repository.java
@@ -4,10 +4,13 @@ public class Repository extends DatabaseConnection {
     
     public Repository() throws Exception {
         super();
+        recipes = new RecipeRepository(this);
+        ingrediences = new IngredienceRepository(this);
+        recipeIngrediences = new RecipeIngredienceRepository(this);
     }
 
-    public RecipeRepository recipes = new RecipeRepository();
-    public IngredienceRepository ingrediences = new IngredienceRepository();
-    public RecipeIngredienceRepository recipeIngrediences = new RecipeIngredienceRepository();
+    public RecipeRepository recipes;
+    public IngredienceRepository ingrediences;
+    public RecipeIngredienceRepository recipeIngrediences;
     
 }
