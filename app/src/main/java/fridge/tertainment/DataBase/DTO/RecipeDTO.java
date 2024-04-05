bf521fa4d995da0f8e81562b71407a28e5ed4b2a
chrnigi <147508181+chrnigi@users.noreply.github.com>
2024-04-05 12:15:13 +0200

    .Implemented some of the functions, and added class members to better represent our database.

--- app/src/main/java/fridge/tertainment/DataBase/DTO/RecipeDTO.java
+++ app/src/main/java/fridge/tertainment/DataBase/DTO/RecipeDTO.java
@@ -6,12 +6,31 @@ public class RecipeDTO extends DTO1 {
     public String text;
     public int amount;
 
-    public RecipeDTO (int _id, String _name, String _text, int _amount) { 
+    public int cookingTime;
+    public int calories;
+
+    public String category;
+
+
+    public RecipeDTO (int _id, String _name, String _text, int _amount, int _calories, int _cookingTime) {
         id = _id; 
         name = _name;
         text = _text;
         amount = _amount;
+        calories = _calories;
+        cookingTime = _cookingTime;
     }
+
+    // Constructor for creating a new RecipeDTO.
+    public RecipeDTO (String _name, String _text, int _amount, int _calories, int _cookingTime) {
+        id = 0;
+        name = _name;
+        text = _text;
+        amount = _amount;
+        calories = _calories;
+        cookingTime = _cookingTime;
+    }
+
 
     @Override
     public String toString(){
